32179df237fce732c2d4151057c9c0d070dc17e5
Alex Roberts <alex@creativecommons.org>
2009-04-23 16:26:26 -0700

    Added print css

--- page.tpl.php
+++ page.tpl.php
@@ -17,6 +17,7 @@
 
   <?php print $head ?>
   <?php print $styles ?>
+  <link href="<?php print $theme_path; ?>/print.css" rel="stylesheet" media="print" type="text/css" />
   <link href="http://creativecommons.org/includes/progress.css?<?= rand()?>" rel="stylesheet" type="text/css" />
   <!--[if IE]><link rel="stylesheet" type="text/css" media="screen" href="<?php print $theme_path ?>/style-ie.css" />
    <style type="text/css">
