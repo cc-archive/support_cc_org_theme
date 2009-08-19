caa060af1acfd30fc723cc485ecfed5836f75530
Alex Roberts <alex@creativecommons.org>
2009-08-19 22:30:00 +0000

    Implemented layout for store items

--- node-product.tpl.php
+++ node-product.tpl.php
@@ -9,25 +9,33 @@
   <?php if ($page == 0){ ?>
 	<h2><a href="<?= $node_url ?>" title="<?= $title ?>"><?= $title ?></a></h2>
   <?php } ?>
-  <?/*= $picture */?>
+  <?/*= $picture */
+	$hero_image = end($node->field_image_cache);
+  ?>
   <div class="content">
  	<table>
 	  <tr>
-		<td>[shirt graphic]</td>
+		<td><img src="/sites/default/files/imagecache/product_hero/<?php echo $hero_image['filepath'];?> "/> </td>
 		<td>
 		  <h2><?php print uc_currency_format($node->sell_price); ?></h2>
    		  <p><?php print $node->content['add_to_cart']['#value']; ?></p>
 		</td>
 	  </tr>
-	  <tr colspan="2">
-	  	<td>{shirt thumbs}</td>
+	  <tr>
+		<td colspan="2">
+<?php foreach ($node->field_image_cache as $images) { 
+		if ($images['list'] == "1") { ?>
+		<img src="/sites/default/files/imagecache/product/<?php print $images['filepath']; ?>" alt="<?php print $images['data']['title']; ?>" />
+<?php } } ?>
+		<td>
 	  </tr>
 	</table>
 
   </div>
 </div>
-
+<?/*
 <pre>
 <?php // this will print the $node array at the bottom of each node take it out when finished ?>
 <?php var_dump($node); ?>
 </pre>
+*/?>
