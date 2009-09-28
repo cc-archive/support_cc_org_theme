cfa1b24479c4c2a19324fd7345c31fbcb11f3192
Alex Roberts <alex@creativecommons.org>
2009-09-28 23:08:56 +0000

    Added support for Choose Your Own Amount donations. Moved the dialog box configuration to its own function.

--- js/oneclick.js
+++ js/oneclick.js
@@ -1,17 +1,10 @@
 var recurringAmount = 0.0;
 
-function oneClick(e) {
-  e.queryString = jQuery.queryString (e.href);
-
-  // Get the donation level, so we can show/hide things based on it
-  var donation = e.queryString.amount;
-
-	// Multiply up the donation amount if it's a preset recurring amount
-  if (e.queryString.recur) { donation = donation * 12; }
-
-  (donation > 25) ? $('#premiums').show() : $('#premiums').hide();
-  (donation > 25) ? $("#giftCheck").val(["yes"]) : $("#giftCheck").val(["no"]);
-  (donation > 25) ? $('#tshirtSize').show() : $('#tshirtSize').hide();
+function oneClickDialogSetup(e) {
+	console.log(e);
+	(e.donation > 25) ? $('#premiums').show() : $('#premiums').hide();
+  (e.donation > 25) ? $("#giftCheck").val(["yes"]) : $("#giftCheck").val(["no"]);
+  (e.donation > 25) ? $('#tshirtSize').show() : $('#tshirtSize').hide();
 
   if (e.queryString.recur == 1) {
     $("#recur_annual").attr("checked", "checked"); 
@@ -23,11 +16,11 @@ function oneClick(e) {
     $("#recur_infinite").attr("checked", ""); 
   }
   
-  if (donation >= 100) {
+  if (e.donation >= 100) {
     if (e.queryString.recur) {
       recurringAmount = e.queryString.amount;
     } else {
-      recurringAmount = parseFloat(donation / 12).toFixed(2);
+      recurringAmount = parseFloat(e.donation / 12).toFixed(2);
     }
 
     // Fill in the popup's spans with the monthly amount
@@ -41,6 +34,42 @@ function oneClick(e) {
   $("#shirtError").html("");
  
   $("#optout").attr("checked", "");
+	
+}
+
+function oneClick(e) {
+  // Handle "Choose your own amount"
+  if (e.type == "submit") {
+		var e = new Object();
+    e.href = $(':input[name=base_href]').val() + "?" + "amount=" + $(':input[name=donation]').val();
+		e.queryString = jQuery.queryString (e.href);
+		
+		// Check if we're splitting, recurring forever, and the donation was >=75
+		// split: recur=1
+		// perm_recur: recur=2
+		if (($(':input[name=split]').attr('checked') || $(':input[name=perm_recur]').attr('checked')) && e.queryString.amount >= 75) {
+			if ($(':input[name=split]').attr('checked')) {
+				e.queryString.recur = 1;
+			} 
+			if ($(':input[name=perm_recur]').attr('checked')) { 
+				e.queryString.recur = 2;
+			}
+			e.queryString.amount = parseFloat(e.queryString.amount / 12).toFixed(2)
+		}
+  } else {
+		e.queryString = jQuery.queryString (e.href);
+	}
+  
+  // Get the donation level, so we can show/hide things based on it
+  var donation = e.queryString.amount;
+
+	// Multiply up the donation amount if it's a preset recurring amount
+  if (e.queryString.recur) { donation *= 12; }
+
+	// Save the final total donation amount
+	e.donation = donation;
+	
+	oneClickDialogSetup(e);
 
   $('#moreOptions').dialog('option', 'donateElement', e);
   $('#moreOptions').dialog('open');
