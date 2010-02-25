2649f61a031525c0279f0ef5665f36e32c629f8c
Alex Roberts <alex@creativecommons.org>
2010-02-25 00:07:28 +0000

    Removed conditional serving of theme . A recent update broke the ordering of jquery script loading (breaking the popup in the process), when a pcpid is defined on a donate page.

--- page.tpl.php
+++ page.tpl.php
@@ -17,16 +17,16 @@
   
   <?php print $head ?>
   <?php print $styles ?>
-
+  <?php print $scripts;  ?>
+  
   <?php if ((strpos ($node->title, "Donate") === 0) || (strpos($_SERVER["REQUEST_URI"], "pcp"))) { ?>
-  <?php if (strpos($_SERVER["REQUEST_URI"], "pcp") < 1) { print $scripts; } ?>
   <script type="text/javascript" src="<?php print $theme_path; ?>/js/jquery.js"></script>
   <script type="text/javascript" src="<?php print $theme_path; ?>/js/jquery-ui.js"></script>
   <script type="text/javascript" src="<?php print $theme_path; ?>/js/jquery.ba-url.min.js"></script>
   <script type="text/javascript" src="<?php print $theme_path; ?>/js/oneclick.js"></script>
   <link rel="stylesheet" href="<?php print $theme_path; ?>/css/ui-lightness/jquery-ui-1.7.2.custom.css" type="text/css" media="screen" title="no title" charset="utf-8" />
-  <?php } else { print $scripts; }
-  if (strpos($_SERVER["REQUEST_URI"], "pcp")) { print $scripts; } 
+  <?php } 
+//  if (strpos($_SERVER["REQUEST_URI"], "pcp")) { print $scripts; } 
   ?>
   
   <!-- Fancybox - http://fancybox.net -->
