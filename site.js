e9111255b507fdba3afbebdd26e0ebc7824f0b9d
Alex Roberts <alex@creativecommons.org>
2010-06-30 22:47:55 +0000

    Removed campaign banner assets

--- site.js
+++ site.js
@@ -1,30 +1,3 @@
-function bannerHtml() {
-    var banner = new Array(3); for (i = 0; i < banner.length; ++i) banner[i] = new Array(2);
-    banner[0][0] = "&utm_medium=banner_1"; banner[0][1] = "Help the world harness the power of Creative Commons.";
-    banner[1][0] = "&utm_medium=banner_2"; banner[1][1] = "Nothing is more powerful than shared knowledge.";
-    banner[2][0] = "&utm_medium=banner_3"; banner[2][1] = "Ignite openness and innovation around the world.";
-    return banner;
-}
-function thundercats() {
-    if ((location.href.match(/http\:(.*)?creativecommons.org\/$/) && !location.href.match(/pport.creativecommons.org\/$/)) || location.href.match(/creativecommons.org\/donate/)) return;
-
-    var i = Math.floor(Math.random() * 3);
-    var banners = bannerHtml();
-
-    var d = document.createElement("div");
-    var mainContent = document.getElementById("globalWrapper");
-    d.setAttribute('style', 'font-size: 22px; font-family: "helvetica neue", arial, sans-serif; line-height:1; text-shadow: 0 1px 0 #45c5ea; color: #ffff00; padding: 12px 0; border-bottom: 1px solid rgb(120, 159, 44); margin-top: -1px; background: #35afe3; z-index:1000;');
-    d.innerHTML = '<a href="/donate?utm_campaign=catalyst' + banners[i][0] + '&utm_source=support&utm_medium=banner" style="color:#ffff00; text-decoration:none;"><strong><span style="color:#000">Catalyst Grants:</span> <span style="text-shadow: 0 -1px 0 #258ab9">' + banners[i][1] + '</span></strong> <span style="color:#000">&mdash; Donate Now</span></a>';
-    mainContent.parentNode.insertBefore(d, mainContent);
-}
-
-if (typeof window.addEventListener !== 'undefined') {
-    window.addEventListener('load', thundercats, false);
-} else {
-    window.attachEvent('onload', thundercats);
-}
-
-
 if (typeof YAHOO != "undefined") { 
 
 YAHOO.namespace("cc.site");
