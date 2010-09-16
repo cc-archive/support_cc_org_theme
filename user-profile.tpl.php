95de1c84aa6e03d769517525a731ac63ace1e27e
JED3 <jed@jedpad.(none)>
2010-09-16 14:49:35 -0700

    Styling the civi forms and incorporating more infor on profile pages

--- user-profile.tpl.php
+++ user-profile.tpl.php
@@ -19,24 +19,21 @@
 <? if ($user->uid == $account->uid && in_array('Commoner', array_values($user->roles))) { 
          // is the user logged in at their own profile page ?>
 
-       <div>
+       <div class="green-box">
          <h4>Your OpenID URL</h4>
          <input onclick="this.select()" readonly="readonly" value="https://creativecommons.net/<?=$user->name?>" type="text">
-
 		<p>
 		  <small>
 		    Use this URL to log into OpenID enabled sites.
-		    
 		    <strong>
 		      To reduce risk of attack we  recommend using HTTPS for your OpenID URL.
 		    </strong>
-		    
 		  </small>
 		</p>
        </div>
        <br />
 
-       <div>   
+       <div class="green-box">   
          <h4>Place this badge on your website</h4>
           <? print $account->content['badge']['#children'] ?>
        </div>
@@ -44,17 +41,47 @@
 
 <? } ?>
 
-   <div>
+<? if ( $account->content['works'] ) { ?>
+   <div class="green-box">
      <h4>Works I have licensed</h4>
      <? print $account->content['works']['#children'] ?>
    </div>
-
-
+<? } ?>
 
 </div>
+
+<? if ($user->uid == $account->uid) { ?>     
+   <h2>Profile Information</h2>
+     <small>Information published on your public profile.</small>
+     <p><strong>Your name</strong></p>
+     <p><? print $account->profile_name ?></p>
+     <p><strong>Your location</strong></p>
+     <p><? print $account->profile_location ?></p>
+     <p><strong>Your homepage</strong></p>
+     <p><? print $account->profile_homepage ?></p>
+     <p><strong>Your story</strong></p>
+     <p><? print nl2br($account->profile_story) ?></p>
+     &raquo; <a href="/<?=$account->name?>/edit/Profile%20Information">Edit 'Profile Information'</a>
+   
+   <div class="divider">&nbsp;</div>
+
+   <h2>Name and Address</h2>
+     <small>This information is for the internal use of CC and will not be made public on your profile.</small>
+      <? print $account->content['Name and Address']['#children'] ?>
+
+   <div class="divider">&nbsp;</div>
+
+   <h2>Discussion Lists</h2>
+     <small>Subscribe to our different mailing lists.</small>
+     <? print $account->content['Email Lists']['#children'] ?>
+
+<? } else { ?>
+
    <h2>My CC Story</h2>
-<p>
-<?php print nl2br($account->profile_story) ?>
-</p>
+   <p>
+     <?php print nl2br($account->profile_story) ?>
+   </p>
+
+<? } ?>
 </div>
 
