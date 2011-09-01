ce4be941324014905093bcab592fe6bbb037b820
Nathan Kinkade <nkinkade@creativecommons.org>
2011-09-01 17:24:30 +0000

    Added Analytics code to page template so we can use PHP, and removed it from the Drupal footer block in admin web interface.

--- page.tpl.php
+++ page.tpl.php
@@ -223,6 +223,25 @@ if ($title == 'Access denied' && !$logged_in) {
 <!-- footer -->
  <?php print $closure;?>
   <script type="text/javascript">var cj = jQuery.noConflict(); $ = cj;</script>
+
+<?php
+  if ( ! (preg_match('/openid\/provider/', $_SERVER['REQUEST_URI']) ||
+       preg_match('/openid\/provider/', $_SERVER['HTTP_REFERER'])) ) {
+    $ga_code = <<<GACODE
+  <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  </script>
+  <script type="text/javascript">
+    var pageTracker = _gat._getTracker("UA-2010376-4");
+    pageTracker._trackPageview();
+  </script>
+GACODE;
+    echo $ga_code;
+  }
+?>
+
+
  </body>
 </html>
 
